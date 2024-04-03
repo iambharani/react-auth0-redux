33c905edeba57b94e6cba7e48c744f4d1a214a72
iambharani <iambharanivelu@gmail.com>
2024-04-03 17:29:04 +0530

    Url Issue Fix

--- src/index.js
+++ src/index.js
@@ -4,20 +4,21 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
+import config from './config';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-
+console.log(`${config.homeUrl}/profile`);
 root.render(
   <React.StrictMode>
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirectUri={window.location.origin + '/profile'}
-    >
+      redirectUri = {`${config.homeUrl}/profile`}
+      >
       <Provider store={store}>
         <App />
       </Provider>
